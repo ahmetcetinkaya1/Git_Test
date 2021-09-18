41e818d48ef464f85c7be656b380748a69d7c875
Ahmet <111actnk@gmail.com>
2021-09-18 15:37:14 +0200

    ikinci surum

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,5 +2,6 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("GITTEN MERHABA");
         System.out.println("GIThubdan MERHABA");
+        System.out.println("ee yine gitden merhaba");
     }
 }
